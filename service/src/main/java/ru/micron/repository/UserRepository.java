ee27d26c39fa6063cf95bdb74f5ee2f8df24f058
m1cron <m1cron4900@gmail.com>
2021-10-19 17:13:22 +0300

    lalala

--- service/src/main/java/ru/micron/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.micron.repository;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.micron.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {}
---
+++ src/main/java/ru/vtb/ievt/Application.java
@@ -0,0 +1,8 @@
+package ru.vtb.ievt;
+
+public class Application {
+
+	public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
